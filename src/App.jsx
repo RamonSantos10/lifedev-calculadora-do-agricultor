132020d24e4442855bec2eb80a1b8093e1006099
HugoM1randa <hugo.barroso@fatec.sp.gov.br>
2025-05-13 11:47:20 -0300

    interligação

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,29 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import './App.css'
+import { AuthProvider } from './components/context/AuthContex'
+import {onAuthStateChanged} from 'firebase/auth'
+import { useState, useEffect } from 'react'
+import { useAuthentication } from './hooks/useAuthentication'
 
 function App() {
+const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication 
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, () => {
+      setUser(user)
+    })
+  }, [auth])
+
+if(loadingUser){
+    return <p>Carregando...</p>
+  }
 
   return (
-    <>
+    <AuthProvider value={user}>
     <div>
       <BrowserRouter>
       <Navbar/>
@@ -23,7 +41,7 @@ function App() {
       <Footer/>
       </BrowserRouter>
     </div>
-    </>
+    </AuthProvider>
   )
 }
 
