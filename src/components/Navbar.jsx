f8ff185233e80d771bf2cbb56a69bbef7ac5b6ca
isabelaneves1 <nevesisabela03@gmail.com>
2025-05-13 11:09:20 -0300

    alteracoesprova

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import { useState } from 'react'
 import styles from './Navbar.module.css'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   const [user, setUser] = useState({ name: 'Hugo', idade: 19 })
@@ -9,14 +8,17 @@ const Navbar = () => {
     <>
       <nav className={styles.navbar}>
         <ul className={styles.link_list}>
-          <li>Home</li>
+          <Navlink to ="/" className={styles.brand} activeClassName=
+          {styles.active}>
+            <li><span>Life</span>Dev</li>
+          </Navlink>
+          <NavLink to="/login" className={styles.link} activeClassName={styles.active}>
+          <li>Login</li>
+          </NavLink>
+          <NavLink to="/register" className={styles.link} activeClassName={styles.active}>
           <li>Register</li>
-          <li>New Post</li>
-          <li>Dasboard</li>
-          <li>About Us</li>
-          <li>
-            <button className={styles.logout}>Exit</button>
-          </li>
+          </NavLink>
+          <button className={styles.exit}>Exit</button>
         </ul>
       </nav>
     </>
